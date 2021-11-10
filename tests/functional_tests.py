1cde510f56c4d25f0c145249a8775cca0038f972
Mihnea Boca <mxb1050@cs.bham.ac.uk>
2021-11-10 18:54:59 +0000

    Created failed functional tests

---
+++ tests/functional_tests.py
@@ -0,0 +1,25 @@
+import os
+
+from selenium import webdriver
+import unittest
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+class NewTest(unittest.TestCase):
+
+    def setUp(self):
+        print(os.path.dirname(os.path.abspath(__file__)) + '\chromedriver.exe')
+        self.browser = webdriver.Chrome(executable_path=os.path.dirname(os.path.abspath(__file__)) + '\chromedriver.exe')
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_access_website(self):
+
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn('Index', self.browser.title)
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
