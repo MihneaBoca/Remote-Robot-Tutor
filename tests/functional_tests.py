2c9aaa56631ff184d60477a209ca04af9a1e6eb0
Mihnea Boca <mxb1050@cs.bham.ac.uk>
2021-11-15 14:27:38 +0000

    Added failed functional tests

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -1,23 +1,28 @@
 import os
-
 from selenium import webdriver
 import unittest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 
 
 class NewTest(unittest.TestCase):
 
     def setUp(self):
-        self.browser = webdriver.Chrome(executable_path=os.path.dirname(os.path.abspath(__file__)) + '\chromedriver.exe')
+        self.browser = webdriver.Chrome(
+            executable_path=os.path.dirname(os.path.abspath(__file__)) + '\chromedriver.exe')
 
     def tearDown(self):
         self.browser.quit()
 
     def test_access_website(self):
-
         self.browser.get('http://localhost:8000')
 
         self.assertIn('Index', self.browser.title)
+
+        WebDriverWait(self.browser, 5).until(lambda d: d.find_element(By.NAME, 'submit_code'))
+
+        self.browser.find_element(By.NAME, 'submit_code')
 
 
 if __name__ == '__main__':
